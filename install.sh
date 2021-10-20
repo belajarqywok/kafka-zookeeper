a2f8991d8703fd28b6b893a83d4b6d768c2e1a8c
qywok <59132829+belajarqywok@users.noreply.github.com>
2021-10-20 21:34:14 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,9 @@
 apt update && apt upgrade
 apt install openjdk-8-jdk openjdk-8-jre
 
-wget https://www.apache.org/dyn/closer.cgi?path=/kafka/3.0.0/kafka_2.12-3.0.0.tgz
-tar -xzf kafka_2.12-3.0.0.tgz
-rm -irf kafka_2.12-3.0.0.tgz
+wget https://archive.apache.org/dist/kafka/2.3.0/kafka_2.12-2.3.0.tgz
+tar -xzf kafka_2.12-2.3.0.tgz
+rm -irf kafka_2.12-2.3.0.tgz
 cd $(ls | grep kafka) && mv -fin * /bin
+
+
